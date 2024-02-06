556caeec79a4b358b6dbddd2c04135d9b390d427
asttryd <asttrydp@gmail.com>
2024-02-06 13:25:17 -0300

    Primeiro commit

---
+++ src/turma72/MyFirstApplication.java
@@ -0,0 +1,13 @@
+package turma72;
+
+public class MyFirstApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome;
+		nome = "Asttryd";
+		
+		System.out.println("Hello World, essa é a primeira aplicação de: " + nome);
+	}
+
+}
