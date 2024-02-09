c51911bee93302971d63234d79c5547dc61cce5f
asttryd <asttrydp@gmail.com>
2024-02-09 14:15:59 -0300

    Atualizando commit

---
+++ src/turma72/LoopDoWhile.java
@@ -0,0 +1,24 @@
+package turma72;
+
+import java.util.Scanner;
+
+public class LoopDoWhile {
+
+	public static void main(String[] args) {
+		int numero, resultado, contador = 0;
+		Scanner leia = new Scanner(System.in);
+		
+		do {
+			System.out.println("Digite um número Inteiro: ");
+			numero = leia.nextInt();
+			
+			resultado = numero * 5;
+			System.out.println("\n O resultado da multiplicação é: " + resultado);
+			contador++;
+		} while (contador < 3);
+		
+		leia.close();
+
+	}
+
+}
---
+++ src/turma72/LoopWhile.java
@@ -0,0 +1,36 @@
+package turma72;
+
+import java.util.Scanner;
+
+public class LoopWhile {
+
+	public static void main(String[] args) {
+		String continua = "s";
+		int numero1, numero2, resultado;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		while(continua.equals("s")) {
+			System.out.println("\n+++++++++++++ MENU +++++++++++++");
+			System.out.println("Deseja somar dois valores?");
+			System.out.println("Digite s para sim OU digite n para não: ");
+			continua = leia.next();
+			System.out.println("Digite o primeiro valor: ");
+			numero1 = leia.nextInt();
+			
+			System.out.println("Digite segundo valor: ");
+			numero2 = leia.nextInt();
+			
+			resultado = numero1 + numero2;
+			
+			System.out.println("O resultado da soma é: " + resultado);
+			
+			
+			System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
+			
+			leia.close();
+		}
+		
+	}
+
+}
